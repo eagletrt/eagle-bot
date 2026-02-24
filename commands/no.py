7cb07762e1da995d38671b5b4ba8864b4fd62c62
mavgrik <79848516+mavgrik@users.noreply.github.com>
2026-02-24 14:57:57 +0100

    feat: implement /no command with username validation and logging

---
+++ commands/no.py
@@ -0,0 +1,30 @@
+import logging
+import requests
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def no(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a greeting and logs who started the bot."""
+
+    # Check if the command is used in a message context
+    if update.edited_message or update.message_reaction:
+        return
+
+    # Ensure the user has a Telegram username
+    username = update.effective_user.username
+    if not username:
+        logging.warning("commands/no - User without username attempted to use /no command")
+        await update.message.reply_html("You need a Telegram username to use this command.")
+        return
+    
+    # Whitelist check
+    if context.bot_data['config']['Features']['Whitelist'] and not context.bot_data['whitelist'].is_user_whitelisted(username, context.bot_data['config']['Whitelist']['General']):
+        logging.warning(f"commands/no - Unauthorized /no attempt by @{username}")
+        return
+
+    url = context.bot_data['config']['Settings']['NAAS_API_URL']
+    response = requests.get(url, timeout=5).json()
+
+    await update.message.reply_html(f"@{username} wanted to say:\n{response['reason']}")
+
+    return
